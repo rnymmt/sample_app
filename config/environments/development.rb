ad4b01d13ab83c9942c496ecf8ee1843383b81bd
EC2 Default User <ec2-user@ip-172-31-28-28.ap-northeast-1.compute.internal>
2023-12-18 14:21:31 +0000

    10-5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "aa3f875fb8b94e9e91b23bdac81426f7.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
